01f227541378714b34d146c9df3e259db363c692
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-06-04 01:22:07 +0300

    refactor app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gold_salver_prices/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GoldSalverPricesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
