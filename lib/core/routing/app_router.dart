c9aec063f02820445f37c2dd1f3cd0c931f89f7f
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-06-07 11:27:52 +0300

    add goldScrren to appROuter

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gold_salver_prices/core/presentation/home_screen.dart';
 import 'package:gold_salver_prices/core/routing/routes.dart';
+import 'package:gold_salver_prices/features/gold/presentation/screens/gold_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.homeScreen:
         return MaterialPageRoute(builder: (context) => HomeScreen());
+      case AppRoutes.goldScreen:
+        return MaterialPageRoute(builder: (context) => GoldScreen());
+      // case AppRoutes.silverScreen:
+      //   return MaterialPageRoute(builder: (context) => SilverScreen());
       default:
         return MaterialPageRoute(builder: (context) => Scaffold(
           appBar: AppBar(
